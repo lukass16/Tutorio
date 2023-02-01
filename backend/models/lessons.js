2cda349d98a9b01627dc357d51e6b5de4903c264
Arvids <52276695+XeMoPHOs@users.noreply.github.com>
2023-02-01 14:53:02 +0200

    added lessons.js model

--- backend/models/lessons.js
+++ backend/models/lessons.js
@@ -1,17 +1,14 @@
-//const mongoose = require('mongoose');
-//
-//const Schema = mongoose.Schema;
-//
-//const teacherSchema = new Schema({
-//  name: { type: String, required: true },
-//  surname: { type: String, required: true },
-//  email: { type: String, required: true },
-//  phone: { type: String, required: true },
-//  description: { type: String, required: true },
-//  password: { type: String, required: true },
-//  subjects: [{type: String, required: true}],
-//  lessons: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Lesson' }]
-//});
-//
-//module.exports = mongoose.model('Lesson', teacherSchema);
-//
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const lessonSchema = new Schema({
+  subject: { type: String, required: true },
+  comment_from_st: { type: String, required: true },
+  date: { type: String, required: true },
+  place: { type: String, required: true },
+  lessonID: { type: mongoose.Types.ObjectId, required: true, ref: 'Student' },
+  studentID: { type: mongoose.Types.ObjectId, required: true, ref: 'Teacher' }
+});
+
+module.exports = mongoose.model('Lesson', teacherSchema);
