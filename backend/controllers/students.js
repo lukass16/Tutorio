4dca0ee7dec07c9ab93b216dbc3dbb386b3e3aa4
Arvids <52276695+XeMoPHOs@users.noreply.github.com>
2023-02-01 14:39:41 +0200

    moved and added student.js

--- backend/controllers/students.js
+++ backend/controllers/students.js
@@ -1,95 +1,113 @@
-let DUMMY_STUDENTS = [
-    {
-      id: 1,
-      name: "Tony",
-      surname: "Stark",
-      email: "hero@fake.com",
-      phoneNumber: "+371 20001607",
-      description: "Likes to solve mysteries",
-      password: "Watson",
-      lessons: [],
-    },
-    {
-      id: 2,
-      name: "Nick",
-      surname: "Fury",
-      email: "hero2@fake.com",
-      phoneNumber: "+371 22201607",
-      description: "Likes to solve mysteries",
-      password: "Sherlock",
-      lessons: [],
-    },
-  ];
-  
-  exports.getStudent = (req, res, next) => {
-    const studentId = req.params.studentId;
-    student = DUMMY_STUDENTS.find((student) => {
-      return studentId === student.id.toString();
-    });
-    res.status(200).json({ student: student });
-  };
-  
-  exports.signup = (req, res, next) => {
-    const {
-      id,
-      name,
-      surname,
-      email,
-      phoneNumber,
-      description,
-      password,
-      lessons,
-    } = req.body;
-  
-    newStudent = {
-      id: id,
-      name: name,
-      surname: surname,
-      email: surname,
-      phoneNumber: phoneNumber,
-      description: description,
-      password: password,
-      lessons: lessons,
-    };
-  
-    DUMMY_STUDENTS.push(newStudent);
-  
-    res.status(200).json({ student: newStudent });
-  };
-  
-  exports.updateStudent = (req, res, next) => {
-    const {
-      id,
-      name,
-      surname,
-      email,
-      phoneNumber,
-      description,
-      password,
-      lessons,
-    } = req.body;
-  
-    const studentId = req.params.studentId;
-    student = DUMMY_STUDENTS.find((student) => {
-      return studentId === student.id.toString();
-    });
-  
-    student.name = name; // currently updating only name
-  
-    res.status(200).json({ student: student });
-  };
-  
-  
-  exports.deleteStudent = (req, res, next) => {
-    const studentId = req.params.studentId;
-  
-    // removing teacher
-    filtered = DUMMY_STUDENTS.filter(student => {
-      return student.id != studentId;
+const mongoose = require("mongoose");
+
+const Student = require("../models/student");
+
+exports.getStudents = (req, res, next) => {
+  Student.find()
+    .then((students) => {
+      res.status(200).json({ students: students });
     })
-  
-    DUMMY_STUDENTS = filtered;
-   
-    res.status(200).json({ teachers: DUMMY_STUDENTS });
-  };
-  
+    .catch((err) => {
+      console.log(err);
+      const error = new Error(err);
+      return next(error);
+    });
+};
+
+exports.getStudent = (req, res, next) => {
+  const studentId = req.params.studentId;
+
+  Student.findById(studentId)
+    .then((student) => {
+      res.status(200).json({ student: student });
+    })
+    .catch((err) => {
+      console.log(err);
+      const error = new Error(err);
+      return next(error);
+    });
+};
+
+exports.signup = (req, res, next) => {
+  const {
+    id,
+    name,
+    surname,
+    email,
+    phone,
+    password,
+    lessons,
+  } = req.body;
+
+  const newStudent = Student({
+    id: id,
+    name: name,
+    surname: surname,
+    email: email,
+    phone: phone,
+    password: password,
+    lessons: lessons,
+  });
+
+  newStudent
+    .save()
+    .then((newStudent) => {
+      res.status(200).json({ student : newStudent });
+    })
+    .catch((err) => {
+      console.log(err);
+      const error = new Error(err);
+      return next(error);
+    });
+};
+
+exports.updateStudent = (req, res, next) => {
+  const studentId = req.params.studentId;
+
+  const {
+    name,
+    surname,
+    email,
+    phone,
+    password,
+    lessons,
+  } = req.body;
+
+  Student.findById(studentId)
+    .then((student) => {
+      if (!student) {
+        throw "Update failed: student not found!";
+      }
+
+      student.name = name;
+      student.surname = surname;
+      student.email = email;
+      student.phone = phone;
+      student.password = password;
+      student.lessons = lessons;
+
+      return student.save();
+    })
+    .then((student) => {
+      res.status(200).json({ student: student });
+    })
+    .catch((err) => {
+      console.log(err);
+      const error = new Error(err);
+      return next(error);
+    });
+};
+
+exports.deleteStudent = (req, res, next) => {
+  const studentId = req.params.studentId;
+
+  Student.findByIdAndRemove(studentId)
+    .then(() => {
+      res.status(200).json({ message: "Successfuly deleted student!" });
+    })
+    .catch((err) => {
+      console.log(err);
+      const error = new Error(err);
+      return next(error);
+    });
+};
