bd7d143e28917ef10748f1ce4bcf57df1efc9bfa
epebepetepe <elizabete.pliene@gmail.com>
2023-02-02 14:00:06 +0200

    Seperate Student and Teacher App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,66 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
+//import { Route, Routes } from "react-router-dom";{ useState }
 
-import MainNavigation from "./components/navigation/MainNavigation";
-import FindLesson from "./pages/FindLesson";
-import Lessons from "./pages/Lessons";
-import Schedule from "./pages/Schedule";
-import Profile from "./pages/Profile";
-import NotFound from "./pages/NotFound";
-import Signup from "./pages/Signup";
+import StudentApp from "./StudentApp";
+import TeacherApp from "./TeacherApp";
 
-const App = () => {
+
+// import MainNavigation from "./components/navigation/MainNavigation";
+// import FindLesson from "./pages/FindLesson";
+// import Lessons from "./pages/Lessons";
+// import Schedule from "./pages/Schedule";
+// import Profile from "./pages/Profile";
+// import NotFound from "./pages/NotFound";
+// import Signup from "./pages/Signup";
+
+// const TeacherApp  = () => {
+//   return (
+//     <>
+//       <MainNavigation />
+//       <Routes>
+//         <Route path="/" element={<FindLesson />} />
+//         <Route path="/find" element={<FindLesson />} />
+//         <Route path="/lessons" element={<Lessons />} />
+//         <Route path="/schedule" element={<Schedule />} />
+//         <Route path="/profile" element={<Profile />} />
+//         <Route path="/signup" element={<Signup />} />
+//         <Route path="*" element={<NotFound />} />
+//       </Routes>
+//     </>
+//   );
+// };
+// function StudentApp (){
+//   return (
+//     <>
+//       <MainNavigation />
+//       <Routes>
+//         <Route path="/" element={<FindLesson />} />
+//         <Route path="/find" element={<FindLesson />} />
+//         <Route path="/lessons" element={<Lessons />} />
+//         <Route path="/schedule" element={<Schedule />} />
+//         <Route path="/profile" element={<Profile />} />
+//         <Route path="/signup" element={<Signup />} />
+//         <Route path="*" element={<NotFound />} />
+//       </Routes>
+//     </>
+//   );
+// };
+
+
+export default function App () {
+  const [active, setActive] = useState ("")
   return (
-    <>
-      <MainNavigation />
-      <Routes>
-        <Route path="/" element={<FindLesson />} />
-        <Route path="/find" element={<FindLesson />} />
-        <Route path="/lessons" element={<Lessons />} />
-        <Route path="/schedule" element={<Schedule />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </>
+    <div className = "App">
+      <nav>
+        <button onClick = {() => setActive("Student")}>Student</button>
+        <button onClick = {() => setActive("Teacher")}>Teacher</button>
+      </nav>
+      <div>
+      {active === "Student" && <StudentApp/>}
+      {active === "Teacher" && <TeacherApp/>}
+      </div>
+    </div>
   );
 };
 
-export default App;
+
