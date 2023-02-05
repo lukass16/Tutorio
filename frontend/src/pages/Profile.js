7db588e807bd894ab71c1093764914462d209c6c
epebepetepe <elizabete.pliene@gmail.com>
2023-02-05 19:12:52 +0200

    New profile page with different cards

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,10 +1,14 @@
-import * as React from 'react';
+import React, { useState } from "react";
 import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
+
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -39,6 +43,145 @@ function a11yProps(index) {
   };
 }
 
+var Name = "Jessica";
+var Surname = "White";
+
+const Basic = () => {
+
+  
+  const bull = (
+    <Box
+      component="span"
+      sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    >
+      •
+    </Box>
+  );
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+          Logged in as: {Name} {Surname} 
+        </Typography>
+        </CardContent>
+    </Card>
+  );
+}
+
+const MyInfo = () => {
+  const Edit = () => {
+    return (
+      <Card>
+        <form>
+          <label>
+            Name: 
+            <input type="text" name="name"  id = "Name" />
+          </label>
+          <label>
+            Surname: 
+            <input type="text" name="surname" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        </Card>
+        );
+  }
+    const [mode, setMode] = useState ("look");
+    const bull = (
+    <Box
+      component="span"
+      sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    >
+      •
+    </Box>)
+    return(
+    <div>
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          My Info
+        </Typography>
+        <Typography variant="h5" component="div">
+          Name: {Name}
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          Surname: {Surname}
+        </Typography>
+        <Typography variant="body2">
+          Role:
+          <br />
+          {'"Student/Teacher"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" onClick={() => setMode(["editing"])}>Edit Info</Button>
+      </CardActions>
+    </Card>
+    <div>
+    {mode[0] === "looking" && <MyInfo />}
+    {mode[0] === "editing" && <Edit />}
+    </div>
+    </div>
+    );
+}
+
+
+const MyDescription = () => {
+
+  const bull = (
+    <Box
+      component="span"
+      sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    >
+      •
+    </Box>
+  );
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          About Me:
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Edit Info</Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+const MyContacts = () => {
+
+  const bull = (
+    <Box
+      component="span"
+      sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    >
+      •
+    </Box>
+  );
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          E-mail:
+        </Typography>
+        <Typography variant="h5" component="div">
+          Phone Number:
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          Socials:
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Edit Info</Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+
+
 export default function VerticalTabs() {
   const [value, setValue] = React.useState(0);
 
@@ -46,13 +189,8 @@ export default function VerticalTabs() {
     setValue(newValue);
   };
 
-  function contentTemplate() {
-    return (
-    <Button variant="text">Text</Button>
-    );
-}
-
   return (
+    <div><Basic/>
     <Box
       sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 224 }}
     >
@@ -64,20 +202,52 @@ export default function VerticalTabs() {
         aria-label="Vertical tabs example"
         sx={{ borderRight: 1, borderColor: 'divider' }}
       >
-        <Tab label="Item One" {...a11yProps(0)} />
-        <Tab label="Item Two" {...a11yProps(1)} />
-        <Tab label="Item Three" {...a11yProps(2)} />
+        <Tab label="My Info" {...a11yProps(0)} />
+        <Tab label="My Description" {...a11yProps(1)} />
+        <Tab label="My Contacts" {...a11yProps(2)} />
 
       </Tabs>
       <TabPanel value={value} index={0}>
-        Item one
+        <MyInfo/>
       </TabPanel>
       <TabPanel value={value} index={1}>
-        Item Two
+        <MyDescription/>
       </TabPanel>
       <TabPanel value={value} index={2}>
-        Item Three
+        <MyContacts/>
       </TabPanel>
+
     </Box>
+    </div>
   );
 }
+
+
+
+
+// export default function Profile() {
+//   //const [user, setUser] = useState(["Teacher", TEACHERID]); // state contains user type and user ID
+//   return (
+//     <div className="Profile">
+//       {/* <UserContext.Provider value={{ user, setUser }}> */}
+//         {" "}
+//         {/*the user type and user ID will now be globaly available with useContext*/}
+//         <nav>
+//           <Box sx = {{height: "30px", display: "flex", alignItems: "center"}}>
+//             <button onClick={() => setUser(["Student", STUDENTID])}>
+//               Student
+//             </button>
+//             <button onClick={() => setUser(["Teacher", TEACHERID])}>
+//               Teacher
+//             </button>
+//             <h5>{user[0]}</h5>
+//           </Box>
+//         </nav>
+//         <div>
+//           {user[0] === "Student" && <StudentApp />}
+//           {user[0] === "Teacher" && <TeacherApp />}
+//         </div>
+//       {/* </UserContext.Provider> */} 
+//     </div>
+//   );
+// }
