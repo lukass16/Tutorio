e38fd57d1cc8d616f8d19258a616cdeabe15acf5
lukass16 <lukasskellijs@gmail.com>
2023-02-07 12:10:49 +0200

    Add Student lessons fetching

--- frontend/src/pages/Lessons.js
+++ frontend/src/pages/Lessons.js
@@ -46,6 +46,7 @@ const Lesson = (props) => {
   const { user } = useContext(UserContext); // get user for which lessons are displayed
 
   // //* Fetching logic
+  // Note currently since both user share the same "Lessons" component, the fetching logic just uses an if to check which user to fetch the data for
   useEffect(() => {
     console.log("Use Effect has triggered!");
 
@@ -67,7 +68,7 @@ const Lesson = (props) => {
             throw new Error(data.message);
             return;
           }
-          console.log("Successfully fetched lessons");
+          console.log("Successfully fetched Teacher lessons");
           console.log(data.lessons);
           setLessonsList(data.lessons);
         })
@@ -75,28 +76,50 @@ const Lesson = (props) => {
           console.log(err);
         });
     } else if (user[0] == "Student") {
-     console.log("Currently haven't implemented such logic");
+      fetch(`http://localhost:5000/api/lessons/student/${user[1]}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => {
+          resStatus = res.status;
+          return res.json();
+        })
+        .then((data) => {
+          // if response failed
+          if (resStatus === 500) {
+            throw new Error(data.message);
+            return;
+          }
+          console.log("Successfully fetched Student lessons");
+          console.log(data.lessons);
+          setLessonsList(data.lessons);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
   }, [user]);
 
   return (
     <Container align="center">
-      {lessonsList &&
-      <Stack spacing={1}>
-        {lessonsList.map((lesson) => {
-          return (
-            <LessonCard
-              id={lesson._id}
-              it={0}
-              subject={lesson.subject}
-              teacher={lesson.teacher}
-              start={lesson.start}
-              end={lesson.end}
-            />
-          );
-        })}
-      </Stack>
-      }
+      {lessonsList && (
+        <Stack spacing={1}>
+          {lessonsList.map((lesson) => {
+            return (
+              <LessonCard
+                id={lesson._id}
+                it={0}
+                subject={lesson.subject}
+                teacher={lesson.teacher}
+                start={lesson.start}
+                end={lesson.end}
+              />
+            );
+          })}
+        </Stack>
+      )}
     </Container>
   );
 };
