a768e985a4b0045f2db4a42a6defff03c7d3387f
lukass16 <lukasskellijs@gmail.com>
2023-02-13 21:16:21 +0200

    Separate EditModal component

--- frontend/src/pages/components/EditLessonModal.js
+++ frontend/src/pages/components/EditLessonModal.js
@@ -1,33 +1,204 @@
 import React from "react";
 
 import { Box, Button, Typography, TextField } from "@mui/material";
+import { useFormik } from "formik";
 import Modal from "@mui/material/Modal";
 
 const modalStyle = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 700,
-    height: 700,
-    bgcolor: "background.paper",
-    border: "2px solid #000",
-    boxShadow: 24,
-    p: 4,
-  };
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 700,
+  height: 700,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const EditLessonModal = (props) => {
+
+  const handleCloseEditModal = () => {
+    props.setEditing(false);
+    props.setOpenEditModal(false);
+  };
+
+  const handleCancel = (e) => {
+    if (props.editing) {
+      e.preventDefault();
+      const calendarApi = props.cal.current.getApi();
+      calendarApi.unselect();
+      props.setOpenEditModal(false);
+      props.setEditing(false);
+    } else {
+      e.preventDefault();
+      props.setOpenEditModal(false);
+    }
+  };
+
+  const handleDelete = () => {
+    const calendarApi = props.cal.current.getApi();
+
+    let resStatus;
+    fetch(`http://localhost:5000/api/lessons/${props.selectedEventId}`, {
+      method: "DELETE",
+    })
+      .then((res) => {
+        resStatus = res.status;
+        return res.json();
+      })
+      .then((data) => {
+        // if response failed
+        if (resStatus === 500) {
+          throw new Error(data.message);
+          return;
+        }
+
+        // change what needs to be changed on the client side
+        calendarApi.getEventById(props.selectedEventId).remove();
+
+        // testing
+        console.log("Successfully deleted lesson/event");
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    props.setEditing(false);
+    props.setOpenEditModal(false);
+  };
+
+  // using the Formik hook
+  const formik = useFormik({
+    initialValues: {
+      subject: "",
+      place: "",
+      price: "",
+    },
+    onSubmit: (values) => {
+      if (props.editing) {
+        const calendarApi = props.cal.current.getApi();
+        const currentEvent = calendarApi.getEventById(props.selectedEventId);
+
+        const updatedLesson = {
+          subject: values.subject,
+          price: values.price,
+          start: currentEvent.start,
+          end: currentEvent.end,
+          place: values.place,
+          teacherId: props.user[1],
+        };
+
+        let resStatus;
+        fetch(`http://localhost:5000/api/lessons/${props.selectedEventId}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            ...updatedLesson,
+          }),
+        })
+          .then((res) => {
+            resStatus = res.status;
+            return res.json();
+          })
+          .then((data) => {
+            // if response failed
+            if (resStatus === 500) {
+              throw new Error(data.message);
+              return;
+            }
+
+            // change what needs to be changed on the client side
+            currentEvent.setProp("title", values.subject);
+
+            // testing
+            console.log("Successfully updated lesson/event");
+            console.log(data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+
+        //set end edit mode
+        props.setEditing(false);
+
+        props.setOpenEditModal(false);
+
+      } else {
+        const calendarApi = props.cal.current.getApi();
+
+        const newLesson = {
+          subject: values.subject,
+          price: values.price,
+          start: props.selectedEvent.start,
+          end: props.selectedEvent.end,
+          place: values.place,
+          teacherId: props.user[1],
+        };
+
+        // add lesson to database
+        let _id;
+        let resStatus;
+        fetch("http://localhost:5000/api/lessons", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            ...newLesson,
+          }),
+        })
+          .then((res) => {
+            resStatus = res.status;
+            return res.json();
+          })
+          .then((data) => {
+            // if response failed
+            if (resStatus === 500) {
+              throw new Error(data.message);
+              return;
+            }
+            _id = data.lesson._id; // should eventually add better error handling here
+
+            const newEvent = {
+              id: _id,
+              title: values.subject,
+              start: props.selectedEvent.start,
+              end: props.selectedEvent.end,
+              allDay: props.selectedEvent.allDay,
+            };
+
+            calendarApi.addEvent(newEvent);
+
+            // testing
+            console.log("Successfully new lesson/event");
+            console.log(newEvent);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+
+        props.setOpenEditModal(false);
+        calendarApi.unselect();
+      }
+    },
+  });
+
   return (
     <Modal
       open={props.open}
-      onClose={props.onClose}
+      onClose={handleCloseEditModal}
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
     >
       <Box
         sx={modalStyle}
         component="form"
-        onSubmit={props.onSubmit}
+        onSubmit={formik.handleSubmit}
         noValidate
         autoComplete="off"
       >
@@ -39,9 +210,9 @@ const EditLessonModal = (props) => {
             name="subject"
             label="Subject"
             variant="filled"
-            onChange={props.onChange}
-            onBlur={props.onBlur}
-            value={props.subject}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.subject}
           />
           <TextField
             required
@@ -49,9 +220,9 @@ const EditLessonModal = (props) => {
             name="place"
             label="Place"
             variant="filled"
-            onChange={props.onChange}
-            onBlur={props.onBlur}
-            value={props.place}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.place}
           />
           <TextField
             required
@@ -59,15 +230,15 @@ const EditLessonModal = (props) => {
             name="price"
             label="Price"
             variant="filled"
-            onChange={props.onChange}
-            onBlur={props.onBlur}
-            value={props.price}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.price}
           />
         </div>
         <Button type="submit">{props.editing ? "Update" : "Submit"}</Button>
-        <Button onClick={props.handleCancel}>Cancel</Button>
+        <Button onClick={handleCancel}>Cancel</Button>
         {props.editing && (
-          <Button onClick={props.handleDelete} variant="danger">
+          <Button onClick={handleDelete} variant="danger">
             Delete
           </Button>
         )}
--- frontend/src/pages/components/TeacherCalendar.js
+++ frontend/src/pages/components/TeacherCalendar.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { useState, useRef, useEffect, useContext } from "react";
 
-import { useFormik } from "formik";
-import FullCalendar, { formatDate } from "@fullcalendar/react";
+import FullCalendar from "@fullcalendar/react";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
-import { Box, Button, Typography, TextField } from "@mui/material";
-import Modal from "@mui/material/Modal";
+import { Box } from "@mui/material";
 
 import UserContext from "../../util/UserContext";
 import EditLessonModal from "./EditLessonModal";
@@ -26,11 +24,10 @@ const modalStyle = {
 
 const selectedEvent = {};
 let selectedEventId;
-let editing = false;
+let editing;
 
 // https://fullcalendar.io/docs/events-json-feed
 // could potentially use multiple event sources that filter out only active lessons etc and apply custom options to them
-// todo: add custom color rendering
 
 const TeacherCalendar = () => {
   const cal = useRef(); // to access the calendar you must use a reference
@@ -39,6 +36,9 @@ const TeacherCalendar = () => {
   const [openAcceptModal, setOpenAcceptModal] = useState(false);
 
   const { user } = useContext(UserContext);
+
+  const setEditing = (value) => editing = value;
+
 
   useEffect(() => {
     console.log("Use Effect has triggered!");
@@ -90,134 +90,9 @@ const TeacherCalendar = () => {
       });
   }, []);
 
-  // using the Formik hook
-  const formik = useFormik({
-    initialValues: {
-      subject: "",
-      place: "",
-      price: "",
-    },
-    onSubmit: (values) => {
-      if (editing) {
-        const calendarApi = cal.current.getApi();
-        const currentEvent = calendarApi.getEventById(selectedEventId);
-
-        const updatedLesson = {
-          subject: values.subject,
-          price: values.price,
-          start: currentEvent.start,
-          end: currentEvent.end,
-          place: values.place,
-          teacherId: user[1],
-        };
-
-        let resStatus;
-        fetch(`http://localhost:5000/api/lessons/${selectedEventId}`, {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            ...updatedLesson,
-          }),
-        })
-          .then((res) => {
-            resStatus = res.status;
-            return res.json();
-          })
-          .then((data) => {
-            // if response failed
-            if (resStatus === 500) {
-              throw new Error(data.message);
-              return;
-            }
-
-            // change what needs to be changed on the client side
-            currentEvent.setProp("title", values.subject);
-
-            // testing
-            console.log("Successfully updated lesson/event");
-            console.log(data);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-
-        //set end edit mode
-        editing = false;
-
-        setOpenEditModal(false);
-
-        // updating place in database
-        //todo
-      } else {
-        const calendarApi = cal.current.getApi();
-
-        const newLesson = {
-          subject: values.subject,
-          price: values.price,
-          start: selectedEvent.start,
-          end: selectedEvent.end,
-          place: values.place,
-          teacherId: user[1],
-        };
-
-        // add lesson to database
-        let _id;
-        let resStatus;
-        fetch("http://localhost:5000/api/lessons", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            ...newLesson,
-          }),
-        })
-          .then((res) => {
-            resStatus = res.status;
-            return res.json();
-          })
-          .then((data) => {
-            // if response failed
-            if (resStatus === 500) {
-              throw new Error(data.message);
-              return;
-            }
-            _id = data.lesson._id; // should eventually add better error handling here
-
-            const newEvent = {
-              id: _id,
-              title: values.subject,
-              start: selectedEvent.start,
-              end: selectedEvent.end,
-              allDay: selectedEvent.allDay,
-            };
-
-            calendarApi.addEvent(newEvent);
-
-            // testing
-            console.log("Successfully new lesson/event");
-            console.log(newEvent);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-
-        setOpenEditModal(false);
-        calendarApi.unselect();
-      }
-    },
-  });
-
-  const handleCloseEditModal = () => {
-    setOpenEditModal(false);
-    editing = false;
-  };
-
   const handleCloseAcceptModal = () => {
     setOpenAcceptModal(false);
-    editing = false;
+    setEditing(false);
   };
 
   const handleDateSelect = (selected) => {
@@ -227,52 +102,6 @@ const TeacherCalendar = () => {
     selectedEvent.allDay = selected.allDay;
 
     setOpenEditModal(true);
-  };
-
-  const handleCancel = (e) => {
-    if (editing) {
-      e.preventDefault();
-      const calendarApi = cal.current.getApi();
-      calendarApi.unselect();
-      setOpenEditModal(false);
-      editing = false;
-    } else {
-      e.preventDefault();
-      setOpenEditModal(false);
-    }
-  };
-
-  const handleDelete = () => {
-    const calendarApi = cal.current.getApi();
-
-    let resStatus;
-    fetch(`http://localhost:5000/api/lessons/${selectedEventId}`, {
-      method: "DELETE",
-    })
-      .then((res) => {
-        resStatus = res.status;
-        return res.json();
-      })
-      .then((data) => {
-        // if response failed
-        if (resStatus === 500) {
-          throw new Error(data.message);
-          return;
-        }
-
-        // change what needs to be changed on the client side
-        calendarApi.getEventById(selectedEventId).remove();
-
-        // testing
-        console.log("Successfully deleted lesson/event");
-        console.log(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-
-    editing = false;
-    setOpenEditModal(false);
   };
 
   const handleEventClick = (selected) => {
@@ -285,7 +114,7 @@ const TeacherCalendar = () => {
     }
 
     //set start edit mode
-    editing = true;
+    setEditing(true);
     setOpenEditModal(true);
   };
 
@@ -303,34 +132,32 @@ const TeacherCalendar = () => {
     <Box m="20px">
       <EditLessonModal
         open={openEditModal}
-        onClose={handleCloseEditModal}
-        onSubmit={formik.handleSubmit}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        subject={formik.values.subject}
-        place={formik.values.place}
-        price={formik.values.price}
-        handleCancel={handleCancel}
-        handleDelete={handleDelete}
         editing={editing}
+        setEditing={setEditing}
+        setOpenEditModal = {setOpenEditModal}
+        selectedEventId={selectedEventId}
+        selectedEvent={selectedEvent}
+        user={user}
+        cal = {cal}
       />
-      <Modal
-        open={openAcceptModal}
-        onClose={handleCloseEditModal}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+      {/* <Modal
+      open={openAcceptModal}
+      onClose={handleCloseEditModal}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box
+        sx={modalStyle}
+        component="form"
+        onSubmit={formik.handleSubmit}
+        noValidate
+        autoComplete="off"
       >
-        <Box
-          sx={modalStyle}
-          component="form"
-          onSubmit={formik.handleSubmit}
-          noValidate
-          autoComplete="off"
-        >
-          <Button onClick={handleAccept}>Accept</Button>
-          <Button onClick={handleDecline}>Decline</Button>
-        </Box>
-      </Modal>
+        <Button onClick={handleAccept}>Accept</Button>
+        <Button onClick={handleDecline}>Decline</Button>
+      </Box>
+    </Modal>*/}
+     
       <Box display="flex" justifyContent="space-between">
         <Box flex="1 1 100%" ml="15px">
           <FullCalendar
